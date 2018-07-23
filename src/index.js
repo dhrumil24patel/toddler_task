07f43b122d647909dd1bd292e8820256507a2277
Dhrumil <dhrumil24patel@gmail.com>
2018-07-23 11:53:53 +0530

    database and queries initialized

--- src/index.js
+++ src/index.js
@@ -1,16 +1,81 @@
-const { GraphQLServer } = require('graphql-yoga')
+import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
 
-const typeDefs = `
-  type Query {
-    hello(name: String): String
-  }
-`
+import { connectToDB } from '../database';
 
-const resolvers = {
-  Query: {
-    hello: (_, args) => `Hello ${args.name || 'World'}!`,
-  },
-}
+// Start the http server
+const startServer = async () => {
+    const { User } = require('../database/models');
 
-const server = new GraphQLServer({ typeDefs, resolvers })
-server.start(() => console.log(`Server is running at http://localhost:4000`))
+    // GraphQL Types
+    const typeDefs = `
+    type User {
+      _id: ID!
+      name: String
+      password: String
+    }
+
+    type Query {
+      users: [User]
+    }
+
+    type Mutation {
+      addUser(input: UserInput): User
+    }
+
+    input UserInput {
+      name: String!
+      password: String!
+    }
+  `;
+
+    // GraphQL resolvers
+    const resolvers = {
+        Query: {
+            users: async () => {
+                const res = await User.find();
+                return res;
+            },
+        },
+
+        Mutation: {
+            addUser: async(root, args) => {
+                const res = await User.create(args.input);
+                return res;
+            },
+        },
+    };
+
+    // Define a schema
+    const schema = makeExecutableSchema({
+        typeDefs,
+        resolvers,
+    });
+
+    // Initiate express and define routes
+    const app = express();
+    app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+    app.use('/', graphiqlExpress({ endpointURL: '/graphql' }));
+
+    // Initiate the server
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server started on port: ${process.env.PORT || 3000}`);
+    });
+};
+
+// Connecting to DB and then start the server
+const dbConnectAndStartServer = async () => {
+    try {
+        await connectToDB();
+        console.log('Connected to Mongo successfully');
+        startServer();
+    } catch (err) {
+        console.error(`Error connecting to mongo - ${err.message}`);
+        process.exit(1);
+    }
+};
+
+// Entry point
+dbConnectAndStartServer();
